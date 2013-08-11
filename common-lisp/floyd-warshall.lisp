1e6fea03bea86223d38595fa8fb0e4be9126300c
Matt Renaud <mrenaud92@gmail.com>
2013-08-11 15:45:27 -0400

    Update floyd-warshall, cleaner to read.

--- common-lisp/floyd-warshall.lisp
+++ common-lisp/floyd-warshall.lisp
@@ -5,18 +5,17 @@
 ;;;
 ;;; See: http://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
 
-(defun floyd_warshall (dist)
+(defun floyd-warshall (dist)
   "Find all pairs shortest path given by the DIST adjacency matrix."
   (let ((n (1- (array-dimension dist 0))))
     (loop :for k :from 0 :to n :do
        (loop :for i :from 0 :to n :do
           (loop :for j :from 0 :to n :do
-             (when (and (not (zerop (* (aref dist i k) (aref dist k j))))
-                        (/= i j))
-               (when (or (< (+ (aref dist i k) (aref dist k j))
-                            (aref dist i j))
-                         (zerop (aref dist i j)))
-                 (setf (aref dist i j)
-                       (+ (aref dist i k) (aref dist k j)))))))))
-  dist)
-                            
+             (unless (zerop (* (aref dist i k) (aref dist k j)))
+               (unless (equal i j)
+                 (when (or (< (+ (aref dist i k) (aref dist k j))
+                              (aref dist i j))
+                           (zerop (aref dist i j)))
+                   (setf (aref dist i j)
+                         (+ (aref dist i k) (aref dist k j)))))))))
+    dist))
